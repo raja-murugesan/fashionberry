7ee6598ffa7101d73f9a414c84091942291f9358
Raghu <ragmurug@cisco.com>
2016-01-09 10:15:44 +0530

    first commit

---
+++ test.sh
@@ -0,0 +1,6 @@
+curl -X POST \
+ -H "X-Parse-Application-Id: tHsoMuJYBJdZuFaQlKWsFXWe665wmePgXXYfNdOk" \
+ -H "X-Parse-REST-API-Key: 3BgM8FyhlNbH711QE3tPCtDQPKHqktWH8fRB9Kmk" \
+ -H "Content-Type: application/json" \
+ -d '{}' \
+ https://api.parse.com/1/functions/hello
